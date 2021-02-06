8e48a2ee8d15eefb4691f3ac8611a9e68f45563f
Fabio <petrifabius@gmail.com>
2021-02-06 12:21:33 +0100

    up

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,11 +10,6 @@ export default class Header extends React.Component {
             <header id="masthead" className="site-header outer">
               <div className="inner">
                 <div className="site-header-inside">
-                
-                  {_.get(this.props, 'pageContext.site.siteMetadata.header.has_nav', null) && (<React.Fragment>
-                  <button id="menu-open" className="menu-toggle"><span className="screen-reader-text">Menu</span><span className="icon-menu"
-                    aria-hidden="true" /></button>
-                  <nav id="main-navigation" className="site-nav" aria-label="Main Navigation">
                   <div className="site-branding">
                     {_.get(this.props, 'pageContext.site.siteMetadata.header.logo_img', null) && (
                     <p className="site-logo">
@@ -28,22 +23,10 @@ export default class Header extends React.Component {
                     <p className="site-title"><Link to={withPrefix('/')}>{_.get(this.props, 'pageContext.site.siteMetadata.header.title', null)}</Link></p>
                     }
                   </div>
-                    <div className="site-nav-inside">
-                      <button id="menu-close" className="menu-toggle"><span className="screen-reader-text">Menu</span><span className="icon-close"
-                          aria-hidden="true" /></button>
-                      <ul className="menu">
-                        {_.map(_.get(this.props, 'pageContext.site.siteMetadata.header.nav_links', null), (action, action_idx) => {
-                            let pageUrl = _.trim(_.get(this.props, 'pageContext.url', null), '/');
-                            let actionUrl = _.trim(_.get(action, 'url', null), '/');
-                            return (
-                              <li key={action_idx} className={classNames('menu-item', {'current-menu-item': pageUrl === actionUrl, 'menu-button': _.get(action, 'style', null) !== 'link'})}>
-                                <ActionLink {...this.props} action={action} />
-                              </li>
-                            )
-                        })}
-                      </ul>
-                    </div>
-                  </nav>
+                  {_.get(this.props, 'pageContext.site.siteMetadata.header.has_nav', null) && (<React.Fragment>
+                  <button id="menu-open" className="menu-toggle"><span className="screen-reader-text">Menu</span><span className="icon-menu"
+                    aria-hidden="true" /></button>
+
                   </React.Fragment>)}
                 </div>
               </div>
